cfebfbdffff93356a71a7fab108e8606a51d9ce1
Tor Kingdon <tor@hearkitty.com>
2022-01-08 22:48:59 -0700

    code works

--- color-box-maker/src/Box.js
+++ color-box-maker/src/Box.js
@@ -1,18 +1,28 @@
 import React, {useState} from 'react';
 
-const Box = ({ color, width, height, testId }) => {
-    return (
-        <div 
-            className="Box"
-            style={{
-                backgroundColor: color,
-                width: `${width}px`,
-                height: `${height}px`
-            }}
-            data-testid={testId}
-        >
-        </div>
-    )
+const Box = ({ color, width, height, id, removeBox }) => {
+  const testId = `box${id}`;
+  const removeThis = () => { removeBox(id) };
+  return (
+    <div className="Box">
+      <div 
+        className="boxDiv"
+        style={{
+          backgroundColor: color,
+          width: `${width}px`,
+          height: `${height}px`
+        }}
+        data-testid={testId}
+      >
+      </div>
+      <button 
+        className="removeBoxButton"
+        onClick={removeThis}
+      >
+        X
+      </button>
+    </div>
+  )
 }
 
 export default Box;
